cfcd03848b2de9ff17171dc8d3df540801ae9040
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-11-25 16:23:57 +0000

    Remove pep508 syntax - doesn't seem to work on amazonlinux

--- cybersecuritytools/flaskauth/setup.py
+++ cybersecuritytools/flaskauth/setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         "pyjwt",
         "requests",
         "serverless_wsgi",
-        "jsonlogger @ git+https://github.com/alphagov/cybersecuritytools.git@authed-tech-docs#egg=jsonlogger&subdirectory=cybersecuritytools/jsonlogger",
+        "git+https://github.com/alphagov/cybersecuritytools.git@authed-tech-docs#egg=jsonlogger&subdirectory=cybersecuritytools/jsonlogger",
     ],
     python_requires=">=3.6",
 )
