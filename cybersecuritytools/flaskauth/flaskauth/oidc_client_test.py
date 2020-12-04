45da42ef64aa37675975573e51856ba88a90a142
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-12-04 11:48:16 +0000

    Fix get_host test and switch off debug mode in tests

--- cybersecuritytools/flaskauth/flaskauth/oidc_client_test.py
+++ cybersecuritytools/flaskauth/flaskauth/oidc_client_test.py
@@ -10,11 +10,16 @@ from .static_site_wrapper_app import bootstrap
 from .oidc_client import get_host, get_client
 
 
-def test_get_host():
-    app = bootstrap()
-    with app.test_request_context("/"):
-        host = get_host()
-        print(host)
+@pytest.mark.usefixtures("test_ssm_parameters")
+def test_get_host(test_ssm_parameters):
+    ssm_prefix = os.environ.get("SSM_PREFIX")
+    stubber = stubs.mock_config_load_ssm_parameters(ssm_prefix, test_ssm_parameters)
+
+    with stubber, requests_mock.Mocker():
+        app = bootstrap()
+        with app.test_request_context("/"):
+            host = get_host()
+            assert host == "http://localhost/"
 
 
 @pytest.mark.usefixtures("openid_config", "test_ssm_parameters")
