2bd45d73ab66339a6304574a665c24671b099187
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-12-07 15:37:02 +0000

    Add type-hints / ignores for auth_test module

---
+++ cybersecuritytools/flaskauth/auth_test.py
@@ -0,0 +1,27 @@
+import os
+from typing import Dict
+
+import pytest
+import requests_mock  # type: ignore
+
+from .auth import make_default_response
+from .static_site_wrapper_app import bootstrap
+from .tests import stubs
+
+
+@pytest.mark.usefixtures("test_ssm_parameters")
+def test_make_default_response(test_ssm_parameters: Dict[str, str]) -> None:
+    ssm_prefix = os.environ.get("SSM_PREFIX", "/static-site")
+    stubber = stubs.mock_config_load_ssm_parameters(ssm_prefix, test_ssm_parameters)
+
+    with stubber, requests_mock.Mocker():
+        app = bootstrap()
+        with app.test_request_context("/"):
+            response = make_default_response("/")
+            body = response.data.decode()
+            assert response.status_code == 200
+            assert "text/html" in response.content_type  # type: ignore
+            assert "Access granted" in body
+
+        with pytest.raises(FileNotFoundError):
+            make_default_response("/not-found.html")
