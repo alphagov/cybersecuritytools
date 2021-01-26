5500cb9b53622e5040d91ca8c464c54da9390376
detnon <sam.detnon@digital.cabinet-office.gov.uk>
2021-01-26 17:24:14 +0000

    Refactored for readability with the help of @0atman and @akinnane

--- cybersecuritytools/aws_permissions_helper/aws_requests.py
+++ cybersecuritytools/aws_permissions_helper/aws_requests.py
@@ -1,12 +1,37 @@
 import re
 import sys
+from typing import List
 
-pattern = "((?<=DEBUG: Request\s)(\w*\W\w*))"
+ACTION = re.compile("((?<=DEBUG: Request\s)(\w*\W\w*))")
 
-input = sys.argv[1]
 
-with open(input,"r") as raw_log:
-    with open('aws_requests.txt','w') as aws_requests:
-        for sorted_request in sorted(set([re.search(pattern,d).group(1)
-            for d in raw_log if re.search(pattern, d)])):
-                aws_requests.write(sorted_request.replace("/",":")+"\n")
+def main():
+    input = sys.argv[1]
+    with open(input, "r") as raw_log:
+        debug_log = raw_log.readlines()
+
+    requests = extract_requests(debug_log)
+
+    with open("aws_requests.txt", "w") as aws_requests:
+        aws_requests.write(requests)
+
+
+def extract_requests(raw_log: List[str]) -> str:
+    """
+    >>> extract_requests(['2021-01-25T16:25:56.957Z [DEBUG] plugin.terraform-provider-aws_v2.70.0_x4: 2021/01/25 16:25:56 [DEBUG] [aws-sdk-go] DEBUG: Request rds/DescribeDBInstances Details:'])
+    'rds:DescribeDBInstances'
+    """
+    parsed_requests = ""
+    iam_actions = set()
+
+    for line in raw_log:
+        match = re.search(ACTION, line)
+        if match:
+            iam_action = match.group(1).replace("/", ":")
+            iam_actions.add(iam_action)
+
+    return "\n".join(sorted(iam_actions))
+
+
+if __name__ == "__main__":
+    main()
