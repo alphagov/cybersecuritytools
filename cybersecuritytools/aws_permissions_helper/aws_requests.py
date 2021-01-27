0327ed4acb945f935d9751fd1d5d30bec2da70e1
detnon <sam.detnon@digital.cabinet-office.gov.uk>
2021-01-27 09:52:17 +0000

    Removed unused variable

--- cybersecuritytools/aws_permissions_helper/aws_requests.py
+++ cybersecuritytools/aws_permissions_helper/aws_requests.py
@@ -21,7 +21,6 @@ def extract_requests(raw_log: List[str]) -> str:
     >>> extract_requests(['2021-01-25T16:25:56.957Z [DEBUG] plugin.terraform-provider-aws_v2.70.0_x4: 2021/01/25 16:25:56 [DEBUG] [aws-sdk-go] DEBUG: Request rds/DescribeDBInstances Details:'])
     'rds:DescribeDBInstances'
     """
-    parsed_requests = ""
     iam_actions = set()
 
     for line in raw_log:
