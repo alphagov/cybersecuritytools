01ba0a0223ec0fea74deaeb2d3785c4c5fdd2b32
detnon <sam.detnon@digital.cabinet-office.gov.uk>
2021-02-02 02:00:00 +0000

    Added grouped_permissions() to jsonise the services and actions

--- cybersecuritytools/aws_permissions_helper/aws_requests.py
+++ cybersecuritytools/aws_permissions_helper/aws_requests.py
@@ -1,8 +1,12 @@
 import re
 import sys
-from typing import List
+import json
+from typing import List, Dict
+from pprint import pprint
 
 ACTION = re.compile("((?<=DEBUG: Request\s)(\w*\W\w*))")
+SERVICE = re.compile("(\w*(?=\:))")
+PERMISSION = re.compile("((?<=\:)\w*)")
 
 
 def main():
@@ -12,8 +16,8 @@ def main():
 
     requests = extract_requests(debug_log)
 
-    with open("aws_requests.txt", "w") as aws_requests:
-        aws_requests.write(requests)
+    with open("aws_requests.json", "w") as aws_requests:
+        aws_requests.write(grouped_permissions(requests))
 
 
 def extract_requests(raw_log: List[str]) -> str:
@@ -30,6 +34,23 @@ def extract_requests(raw_log: List[str]) -> str:
             iam_actions.add(iam_action)
 
     return "\n".join(sorted(iam_actions))
+
+
+def grouped_permissions(requests: str) -> str:
+    """
+    >>> grouped_permissions('acm:DescribeCertificate\\racm:ListTagsForCertificate\\racm:RequestCertificate\\rec2:AuthorizeSecurityGroupEgress\\rec2:AuthorizeSecurityGroupIngress\\rec2:CreateSecurityGroup')
+    '{"acm": ["DescribeCertificate", "ListTagsForCertificate", "RequestCertificate"], "ec2": ["AuthorizeSecurityGroupEgress", "AuthorizeSecurityGroupIngress", "CreateSecurityGroup"]}'
+    """
+    grouped_permissions = {}
+    for request in requests.splitlines():
+        match_service = re.search(SERVICE, request).group(1)
+        match_permission = re.search(PERMISSION, request).group(1)
+        if match_service not in grouped_permissions:
+            grouped_permissions[match_service] = []
+            grouped_permissions[match_service].append(match_permission)
+        else:
+            grouped_permissions[match_service].append(match_permission)
+    return json.dumps(grouped_permissions)
 
 
 if __name__ == "__main__":
